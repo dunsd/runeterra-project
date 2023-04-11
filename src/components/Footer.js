5643850ab79a2500c4303a9030baa92234c430ab
dunsd <dthomasparker@gmail.com>
2023-04-11 21:10:48 +0100

    Add basic flex styling to main,footer, header
    Added flex styling and centering to basic items on page

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './css/Main.css';
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            Made by Daniel Parker
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './css/Main.css';
+
+const MainContent = () => {
+
+    return (
+        <div className="maincontent">
+            Main Content
+        </div>
+    )
+}
+
+export default MainContent;
