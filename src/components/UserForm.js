a16c956db326b964067481d10e564faf8b30e075
dunsd <dthomasparker@gmail.com>
2023-04-11 22:21:37 +0100

    Add change user button to toggle UserForm
    Add state for visibility of form and add conditional
    formatting for the form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 
 const UserForm = () => {
     
+    const [isVisible, setIsVisible] = useState(false);
+
     const [userName, setUserName] = useState(() => {
         const storedUser = localStorage.getItem("userName");
         const parsedUser = JSON.parse(storedUser);
@@ -18,24 +20,34 @@ const UserForm = () => {
         localStorage.setItem("userName", JSON.stringify(userName))
     }, [userName]);
 
+    const toggleForm = (e) => {
+        setIsVisible(prevIsVisible => !prevIsVisible);
+    }
 
     return (
-        <form 
-            className="userForm"
-            onSubmit={(e) => e.preventDefault()}
-        >
-            <label htmlFor="nameEntry">Username:</label>
-            <input
-                name="nameEntry"
-                type="text"
-                value={userName}
-                onChange={handleUserNameChange}
-                placeholder="Username"
-            />
+        <div className="userDetails">
+            <button className="changeUserBtn" onClick={toggleForm}>Change User</button>
+            {isVisible &&
+                <form 
+                    className="userForm"
+                    onSubmit={(e) => e.preventDefault()}
+                >
+                    <label htmlFor="nameEntry">Username:</label>
+                    <input
+                        name="nameEntry"
+                        type="text"
+                        value={userName}
+                        onChange={handleUserNameChange}
+                        placeholder="Username"
+                    />
+                
 
-            <input type="submit" value="Submit"></input>
+                    <input type="submit" value="Submit"></input>
 
-        </form>
+                </form>
+            }
+
+        </div>
     )
 }
 
