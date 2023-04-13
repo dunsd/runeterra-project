3f62bbe7c75277e892268291e20d266a420b2ebd
dunsd <dthomasparker@gmail.com>
2023-04-13 22:32:49 +0100

    Add Riot user account API functions and display
    Set userinfo to state and display in div

--- src/components/APIDetails.js
+++ src/components/APIDetails.js
@@ -1,14 +1,23 @@
-import React from "react";
-import key from "../services/APIKey";
+import React, { useState } from "react";
 import { fetchUserDetails } from "../services/APICalls";
 
 const APIDetails = () => {
 
-   
-    
+   const [userInfo, setUserInfo] = useState({});
+
+    async function getUser(name) {
+        const userData = await fetchUserDetails(name);
+        setUserInfo(userData);
+        console.log(userInfo);
+    }
+
     return (
         <div>
-            
+            <button onClick={() => getUser("Ulfilas")}>Get User</button>
+            {userInfo.id}
+            <div className="userName">Account Name: {userInfo.name || ""}</div>
+            <div className="userPUUID">PUUID: {userInfo.puuid || ""}</div>
+            <div className="userLevel">Account Level: {userInfo.summonerLevel || ""}</div>
         </div>
     )
 }
--- src/services/APICalls.js
+++ src/services/APICalls.js
@@ -2,8 +2,15 @@ import key from "./APIKey";
 
 async function fetchUserDetails(id) {
     try {
-        const response = fetch('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/$'+id+key);
-        return await response.json();
+        const response = await fetch('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+        +id+'?api_key=' + key,
+        {
+            mode: 'cors',
+        });
+        const data = await response.json();
+        console.log(data);
+        return data;
+        
     }
     catch(error) {
         console.log(error);
