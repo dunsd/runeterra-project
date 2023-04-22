ab2908ea1fdf9b10f4ee7129dd3efd987365fdf4
dunsd <dthomasparker@gmail.com>
2023-04-22 14:59:09 +0100

    Move header css to new file, add navbar links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,25 @@
 import React from "react";
-import "./css/Main.css";
+import "./css/Header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerLeft">
-        <h1>Runeterra Hub</h1>
-        <p>
-          A place to view your Legends of Runeterra match history and stats.
-        </p>
-      </div>
-      <Link to="/profile">
-        Profile
-      </Link>
+      
+        <ul>
+          <li className="homeLink">
+            <Link to="/">
+              <h1>Runeterra Hub</h1>
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      
+      
     </div>
   );
 };
