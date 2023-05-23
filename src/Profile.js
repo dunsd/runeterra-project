33b9f85d3c241e33c39c892132396fff20a1dd51
dunsd <dthomasparker@gmail.com>
2023-05-23 22:30:49 +0100

    Add match history route and test button

--- src/Profile.js
+++ src/Profile.js
@@ -6,7 +6,7 @@ import ProfileContent from "./components/ProfileContent";
 const Profile = () => {
 
   const test = async () => {
-    const result = await fetch(`http://localhost:8000/Ulfilas`, {
+    const result = await fetch(`http://localhost:8000/user/Ulfilas`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -15,12 +15,26 @@ const Profile = () => {
     const data = await result.json();
     console.log(data);
   }
+  const testMatch = async () => {
+    const result = await fetch(`http://localhost:8000/matchhistory/OLAK4Fna1yAyLqMFTJ1gPqICrCGwWEZPSQs9JdWtGtzz1NJezSSH8QMgF2jrtcmk0u5fV7lyPl8Uvw`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    const data = await result.json();
+    console.log(data);
+  }
+  
   return (
     <div className="App">
       <Header />
       <ProfileContent />
       <button onClick={test}>
         Test
+      </button>
+      <button onClick={testMatch}>
+        Test Match
       </button>
       <Footer />
     </div>
--- src/server/routes/lorAPICalls.js
+++ src/server/routes/lorAPICalls.js
@@ -1,17 +1,15 @@
-// import {
-//   fetchMatchHistory,
-//   fetchMatchInfo,
-//   fetchUserDetails,
-// } from "../../services/APICalls";
 const express = require("express");
 
 const key = process.env.REACT_APP_RIOT_KEY;
 
-async function fetchUserDetails(id) {
+const lorRoutes = express.Router();
+
+lorRoutes.get("/user/:username", async (req, res) => {
   try {
+    const user = await req.params.username;
     const response = await fetch(
       "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
-        id +
+        user +
         "?api_key=" +
         key,
       {
@@ -19,37 +17,50 @@ async function fetchUserDetails(id) {
       }
     );
     const data = await response.json();
-    console.log(data);
-    return data;
+    res.json(data);
   } catch (error) {
     console.log(error);
   }
-}
+});
 
-const lorRoutes = express.Router();
-
-lorRoutes.get("/:username", async (req, res) => {
-    try{
-        const user = await req.params.username;
+lorRoutes.get("/matchhistory/:puuid", async (req, res) => {
+    try {
+        const puuid = await req.params.puuid;
         const response = await fetch(
-            "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
-              user +
-              "?api_key=" +
+            "https://europe.api.riotgames.com/lor/match/v1/matches/by-puuid/" +
+              puuid +
+              "/ids?api_key=" +
               key,
             {
               mode: "cors",
             }
+            
           );
           const data = await response.json();
           res.json(data);
+
     }
     catch (error) {
         console.log(error);
     }
-  
-//   const result = fetchUserDetails(user);
-//   console.log(result);
-//   res.json(result);
-});
+})
+
+async function fetchMatchHistory(puuid) {
+    try {
+      const response = await fetch(
+        "https://europe.api.riotgames.com/lor/match/v1/matches/by-puuid/" +
+          puuid +
+          "/ids?api_key=" +
+          key,
+        {
+          mode: "cors",
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 module.exports = lorRoutes;
