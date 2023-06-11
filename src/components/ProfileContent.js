fb51951eb5c7a01790730ba1d2f2403706bff101
dunsd <dthomasparker@gmail.com>
2023-06-11 23:13:12 +0100

    Lint code, EOD commit

--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -5,15 +5,12 @@ import APIDetails from "./APIDetails";
 import SignIn from "./SignIn";
 
 const ProfileContent = () => {
-
+  //current signed in user, if null then don't show API details
   const [user, setUser] = useState("");
 
   return (
     <div className="profileContent">
-      <SignIn
-      user={user}
-      setUser={setUser}
-      />
+      <SignIn user={user} setUser={setUser} />
       <UserForm />
       {user && <APIDetails />}
     </div>
--- src/fbhandles/firestoreFunctions.js
+++ src/fbhandles/firestoreFunctions.js
@@ -1,9 +1,4 @@
-import {
-  setDoc,
-  doc,
-  updateDoc,
-  getDoc,
-} from "@firebase/firestore";
+import { setDoc, doc, updateDoc, getDoc } from "@firebase/firestore";
 import { firestore } from "../firebase_setup/firebase";
 
 //create user with email as the id
@@ -17,25 +12,23 @@ async function newUser(user) {
 //add array of Match History to Users.
 async function addMatchHistory(matches, user) {
   try {
-  console.log(user);
-  const userRef = doc(firestore, "users", user);
-  await updateDoc(userRef, {
-    matches: matches,
-  });
-}
-catch (error) {
-  console.log(error);
-}
+    console.log(user);
+    const userRef = doc(firestore, "users", user);
+    await updateDoc(userRef, {
+      matches: matches,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 //return data assigned to a user
 async function getMatches(user) {
   try {
-  const userRef = doc(firestore, "users", user);
-  const docSnap = await getDoc(userRef);
-  console.log(docSnap.data());
-  }
-  catch (error) {
+    const userRef = doc(firestore, "users", user);
+    const docSnap = await getDoc(userRef);
+    console.log(docSnap.data());
+  } catch (error) {
     console.log(error);
   }
 }
@@ -43,17 +36,16 @@ async function getMatches(user) {
 //check if user exists in firestore
 async function checkUserExists(userName) {
   try {
-  const userRef = doc(firestore, "users", userName);
-  const userSnap = await getDoc(userRef);
-  if (userSnap.exists()) {
-    return userSnap.data();
-  } else {
-    return false;
+    const userRef = doc(firestore, "users", userName);
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) {
+      return userSnap.data();
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
   }
-}
-catch (error) {
-  console.log(error);
-}
 }
 
 export { newUser, addMatchHistory, getMatches, checkUserExists };
